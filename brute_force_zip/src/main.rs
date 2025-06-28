2687e1cdbf94f1b9fb6954a4534f0ba1beb8d1c5
ragmilk <ragmilk914@gmail.com>
2025-06-29 08:34:58 +0900

    fix

--- brute_force_zip/src/main.rs
+++ brute_force_zip/src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("\nPassword found: {}", password);
         let secret = get_content(zip_file_path, password);
         let result = Output { secret };
-        util::post_answer("brute_force_zip", result, true).await?;
+        util::post_answer("brute_force_zip", result, false).await?;
     } else {
         println!("\nPassword not found.");
     }
@@ -43,23 +43,24 @@ fn brute_force_zip(zip_file_path: &str) -> Option<String> {
     let base_archive = zip::ZipArchive::new(cursor).unwrap();
 
     let chars: Vec<char> = "abcdefghijklmnopqrstuvwxyz0123456789".chars().collect();
-    let password_candidates = (4..=6).flat_map(|len| {
-        (0..len)
-            .map(|_| chars.iter())
-            .multi_cartesian_product()
-            .map(|p| p.into_iter().collect::<String>())
-    });
-
-    password_candidates.par_bridge().find_any(|try_password| {
-        let mut archive = base_archive.clone();
-        if let Ok(mut zip) = archive.by_index_decrypt(0, try_password.as_bytes()) {
-            let mut buffer = Vec::new();
-            if zip.read_to_end(&mut buffer).is_ok() {
-                return true;
+    (4..=6)
+        .flat_map(|len| {
+            (0..len)
+                .map(|_| chars.iter())
+                .multi_cartesian_product()
+                .map(|p| p.into_iter().collect::<String>())
+        })
+        .par_bridge()
+        .find_any(|try_password| {
+            let mut archive = base_archive.clone();
+            if let Ok(mut zip) = archive.by_index_decrypt(0, try_password.as_bytes()) {
+                let mut buffer = Vec::new();
+                if zip.read_to_end(&mut buffer).is_ok() {
+                    return true;
+                }
             }
-        }
-        false
-    })
+            false
+        })
 }
 
 fn get_content(zip_file_path: &str, password: String) -> String {
