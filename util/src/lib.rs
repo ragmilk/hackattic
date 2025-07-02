b6df7d16c515cd06140b60cea04a3b9a621efdda
ragmilk <ragmilk914@gmail.com>
2025-07-03 00:23:56 +0900

    add download

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -42,23 +42,10 @@ pub async fn post_answer<T: Serialize + Debug>(
     Ok(())
 }
 
-pub async fn post<T: Serialize + Debug>(
-    problem: &str,
-    result: T,
-    playground: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let access_token =
-        std::env::var("HACKATTIC_ACCESS_TOKEN").expect("Please set HACKATTIC_ACCESS_TOKEN");
-    let client = reqwest::Client::new();
-    let p = if playground { "&playground=1" } else { "" };
-    println!("\nPosting Data...");
-    let _ = client
-        .post(format!(
-            "https://hackattic.com/challenges/{problem}/solve?access_token={access_token}{p}",
-        ))
-        .json(&result)
-        .send()
-        .await?;
-    println!("Posted!");
+pub async fn download(file_path: &str, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = std::fs::File::create(file_path).unwrap();
+    let response = reqwest::get(url).await?;
+    let mut content = std::io::Cursor::new(response.bytes().await?);
+    std::io::copy(&mut content, &mut file)?;
     Ok(())
 }
